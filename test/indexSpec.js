59a154ca2619fa18cd86558e8e1b2021b6185da1
Thomas Strothjohann <thomas@strothjohann.net>
2015-04-22 14:31:15 +0200

    can filter by author

--- test/indexSpec.js
+++ test/indexSpec.js
@@ -12,8 +12,18 @@ describe('blogbook homepage', function() {
     expect(element.all(by.css('.items')).count()).toEqual(9);
   });
 
-  it('should show the title of each post', function() {
+  it('shows the title of each post', function() {
     expect(element.all(by.binding('card.title')).first().getText()).toEqual('Pacman and Instagram');
+  });
+
+  it("shows a read more link - linking back to the blog", function(){
+    expect(element.all(by.binding('card.link')).first()).not.toEqual("");
+  });
+
+  it("can filter by author", function(){
+    element(by.id('profile-tab')).click();
+    element(by.id('profile')).all(by.tagName('div')).first().click();
+    expect(element.all(by.css('.items')).count()).toEqual(1);
   });
 
 });
